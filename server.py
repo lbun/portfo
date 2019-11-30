484b848ca411932c013dc02f0bf933b5cfc4d1b9
mac <luigi.bungaro@gmail.com>
2019-11-30 13:58:00 +0100

    website deployment

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def catch_all(path):
+    print(path)
+    return render_template(path)
+
+# submit form endpoint
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+            try:
+                data = request.form.to_dict()
+                write_to_file(data)
+                write_to_csv(data)
+                return redirect('thankyou.html')
+            except:
+                return 'Something went wrong when accessing the database'
+    else:
+        return 'something went wrong'
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='2', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
